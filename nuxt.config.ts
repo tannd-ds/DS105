3336187c8a41bb237db7c73a3d1f0b67f971a0b0
Tannd <64354200+tannd-ds@users.noreply.github.com>
2023-10-29 03:39:10 +0000

    init pinia module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,7 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
   css: ["~/assets/css/main.css"],
+  modules: ['@pinia/nuxt',],
   postcss: {
     plugins: {
       tailwindcss: {},
